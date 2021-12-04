09061ed33f87438e2130f6d4bda420889121172f
Aaron Ortbals <aaron.ortbals@gmail.com>
2021-12-04 11:38:04 -0600

    2021: Day 4 - Part 1

---
+++ 2021/src/lib/days/04.ts
@@ -0,0 +1,119 @@
+import fs from "fs/promises";
+import { exit } from "process";
+
+function chunk<T>(arr: T[], size: number): T[][] {
+  const res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    const chunk = arr.slice(i, i + size);
+    res.push(chunk);
+  }
+  return res;
+}
+
+function transpose<T>(input: T[][]): T[][] {
+  return input[0].map((_, colIndex) => input.map((row) => row[colIndex]));
+}
+
+interface Cell {
+  value: number;
+  marked: boolean;
+}
+
+type Board = Cell[][];
+
+interface Input {
+  drawnNumbers: number[];
+  boards: Board[];
+}
+
+export async function readInput(fileName: string): Promise<Input> {
+  const lines = (await fs.readFile(fileName))
+    .toString()
+    .split("\n")
+    .filter(Boolean);
+
+  const [drawnNumbers, ...boardLines] = lines;
+
+  return {
+    drawnNumbers: drawnNumbers.split(",").map((n) => parseInt(n, 10)),
+    boards: chunk(
+      boardLines.map((l) =>
+        l
+          .trim()
+          .split(/\s+/)
+          .map((value) => ({ value: parseInt(value, 10), marked: false }))
+      ),
+      5
+    ),
+  };
+}
+
+function checkWin(board: Board) {
+  for (const row of board) {
+    if (row.every((cell) => cell.marked)) {
+      return true;
+    }
+  }
+
+  for (const column of transpose(board)) {
+    if (column.every((cell) => cell.marked)) {
+      return true;
+    }
+  }
+}
+
+function calculateScore(board: Board, lastCalled: number) {
+  const unmarkedSum = board.reduce(
+    (sum, row) =>
+      sum + row.reduce((sum, cell) => sum + (cell.marked ? 0 : cell.value), 0),
+    0
+  );
+
+  return unmarkedSum * lastCalled;
+}
+
+async function part1({ drawnNumbers, boards }: Input) {
+  for (const number of drawnNumbers) {
+    let winner: Board | undefined = undefined;
+
+    boards = boards.map((board) => {
+      let hit = false;
+
+      const nextBoard: Board = board.map((row) =>
+        row.map((cell) => {
+          hit = hit || (!cell.marked && number === cell.value);
+
+          return {
+            value: cell.value,
+            marked: cell.marked || number === cell.value,
+          };
+        })
+      );
+
+      if (hit && checkWin(nextBoard)) {
+        winner = nextBoard;
+      }
+
+      return nextBoard;
+    });
+
+    if (winner) {
+      return calculateScore(winner, number);
+    }
+  }
+}
+
+const fileName = process.argv.slice(2)[0];
+
+if (!fileName) {
+  console.log("Example usage: npx ts-node src/lib/days/04 src/input/04.txt");
+  exit(1);
+}
+
+async function main() {
+  const input = await readInput(fileName);
+  console.log(`Day 4 - Part 1: ${await part1(input)}`);
+  // console.log(`Day 4 - Part 2: ${await part2(input)}`);
+}
+
+main();
