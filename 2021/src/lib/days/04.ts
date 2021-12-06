e9f398525f793552ed9ee94b4855fade51033ed2
Aaron Ortbals <aaron.ortbals@gmail.com>
2021-12-06 16:07:15 -0600

    2021: Day 4 - Part 2

--- 2021/src/lib/days/04.ts
+++ 2021/src/lib/days/04.ts
@@ -74,34 +74,96 @@ function calculateScore(board: Board, lastCalled: number) {
   return unmarkedSum * lastCalled;
 }
 
-async function part1({ drawnNumbers, boards }: Input) {
+function updateBoard(board: Board, num: number): [Board, boolean] {
+  let hit = false;
+
+  const updatedBoard = board.map((row) =>
+    row.map((cell) => {
+      hit = hit || (!cell.marked && num === cell.value);
+
+      return {
+        value: cell.value,
+        marked: cell.marked || num === cell.value,
+      };
+    })
+  );
+
+  return [updatedBoard, hit];
+}
+
+function prettyPrintBoard(board: Board) {
+  return board
+    .map((row) =>
+      row
+        .map((cell) =>
+          !cell.marked
+            ? `\x1b[2m${cell.value < 10 ? ` ${cell.value}` : cell.value}\x1b[0m`
+            : cell.value < 10
+            ? ` ${cell.value}`
+            : cell.value
+        )
+        .join(" ")
+    )
+    .join("\n");
+}
+
+async function part1({ drawnNumbers, ...input }: Input) {
+  // Boards are mutated, make a fresh copy
+  let boards = [...input.boards];
+
   for (const number of drawnNumbers) {
+    // Due ot the nature of the challenge, we assume a single winner.
     let winner: Board | undefined = undefined;
 
-    boards = boards.map((board) => {
-      let hit = false;
-
-      const nextBoard: Board = board.map((row) =>
-        row.map((cell) => {
-          hit = hit || (!cell.marked && number === cell.value);
-
-          return {
-            value: cell.value,
-            marked: cell.marked || number === cell.value,
-          };
-        })
-      );
+    boards.forEach((board, index) => {
+      const [nextBoard, hit] = updateBoard(board, number);
 
       if (hit && checkWin(nextBoard)) {
         winner = nextBoard;
       }
 
-      return nextBoard;
+      boards[index] = nextBoard;
     });
 
     if (winner) {
+      console.log(
+        `On a draw of ${number}, your winner is:\n\n${prettyPrintBoard(
+          winner
+        )}\n`
+      );
+
       return calculateScore(winner, number);
     }
+  }
+}
+
+async function part2({ drawnNumbers, ...input }: Input) {
+  // Boards are mutated, make a fresh copy
+  let boards = [...input.boards];
+
+  for (const number of drawnNumbers) {
+    let winners: number[] = [];
+
+    boards.forEach((board, index) => {
+      const [nextBoard, hit] = updateBoard(board, number);
+      boards[index] = nextBoard;
+
+      if (hit && checkWin(nextBoard)) {
+        winners.push(index);
+      }
+    }, boards);
+
+    if (boards.length === 1 && winners.length === 1) {
+      console.log(
+        `\nOn a draw of ${number}, your winner is:\n\n${prettyPrintBoard(
+          boards[winners[0]]
+        )}\n`
+      );
+
+      return calculateScore(boards[winners[0]], number);
+    }
+
+    boards = boards.filter((_b, i) => winners.indexOf(i) === -1);
   }
 }
 
@@ -115,7 +177,7 @@ if (!fileName) {
 async function main() {
   const input = await readInput(fileName);
   console.log(`Day 4 - Part 1: ${await part1(input)}`);
-  // console.log(`Day 4 - Part 2: ${await part2(input)}`);
+  console.log(`Day 4 - Part 2: ${await part2(input)}`);
 }
 
 main();
